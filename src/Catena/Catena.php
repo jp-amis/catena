a1a70e7df5f7f417c739036db050e9dd165e5a26
João Paulo Pinheiro Teixeira <juaumlol@gmail.com>
2015-05-30 18:36:19 -0300

    base class for initial tests

---
+++ src/Catena/Catena.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jp
+ * Date: 5/30/15
+ * Time: 6:35 PM
+ */
+
+namespace Amis\Catena;
+
+
+class Catena {
+
+    public function __construct() {
+        echo "Test Catena";
+    }
+}
